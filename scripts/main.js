d456c09f0d701374d5a80bbed31e7f586a7a0ef3
darckronoz <crispipe14@gmail.com>
2023-11-04 17:11:18 -0500

    nettype added, subnet restricted

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,58 @@
+const body = document.body;
 // Obtener elementos del DOM
-const subNtwkInput = document.getElementById('subNtwk');
+const subNtwkSelect = document.getElementById('subNtwk');
+const netTypeSelect = document.getElementById('netType');
 const hostInput = document.getElementById('host');
 const generateBtn = document.getElementById('generate');
 const hostResult = document.getElementById('hostrange');
 const subnetsResult = document.getElementById('subnets');
 const subnetsList = document.getElementById('subNetList');
-const body = document.body;
-const net_type = 10;
 
-// Agregar evento al botón Generar
-generateBtn.addEventListener('click', () => {
-    const subnet = parseInt(subNtwkInput.value);
-    const host = parseInt(hostInput.value);
-    hostResult.textContent = `${net_type}.${generateHostRange(subnet, host)}`;
-    showNets(generateSubNetAddress(subnet));
+//
+const net_type_select = [10, 66, 128, 192];
+const subnets_select = [1, 2, 4, 8, 16, 32, 64, 128, 256];
+let subnet = 1;
+let netType = 10;
+let host = 1;
+const nets = [];
+
+// Eventos que se ejecutan al cargar la pagina
+document.addEventListener("DOMContentLoaded", function() {
+    fillSubNetSelect();
+    fillNetTypeSelect();
 });
 
-function generateSubNetAddress(subnet) {
-    const nets = [];
+//llenar la seleccion de sub redes
+function fillSubNetSelect() {
+    subnets_select.forEach(e => {
+        let option = document.createElement('option');
+        option.value = e;
+        option.text = e;
+        subNtwkSelect.appendChild(option);
+    });
+}
+
+//llenar la seleccion de tipos de redes
+function fillNetTypeSelect() {
+    net_type_select.forEach(e => {
+        let option = document.createElement('option');
+        option.value = e;
+        option.text = e;
+        netTypeSelect.appendChild(option);
+    });
+}
+
+//evento generar
+generateBtn.addEventListener('click', () => {
+    netType = netTypeSelect.value;
+    subnet = subNtwkSelect.value;
+    showNets();
+    showHostRange();
+});
+
+//función para generar las subredes
+function generateSubNetAddress() {
+    
     const number = 256/subnet;
     for (let i = 0; i < subnet; i++) {
         nets.push(number*i);
@@ -25,16 +60,27 @@ function generateSubNetAddress(subnet) {
     return nets;
 }
 
-function showNets(nets) {
+//función para mostrar las sub redes
+function showNets() {
+    generateSubNetAddress();
     subnetsList.innerHTML = '';
     for (let i = 0; i < nets.length; i++) {
         const li = document.createElement('li');
-        li.textContent = `${i+1}: ${net_type}.${nets[i]}.0.0`;
+        li.textContent = `${i+1}: ${netType}.${nets[i]}.0.0`;
         subnetsList.appendChild(li);
     }
 }
 
-function generateHostRange(host) {
-    return host+net_type;
+//función para generar los rangos de los hosts
+function generateHostRange() {
+    //To Do
+    console.log('To Do generateHost');
+}
+
+//función que muestra los rangos de red
+function showHostRange() {
+    generateHostRange();
+    //To Do
+    console.log('To Do showHostRange');
 }
 
