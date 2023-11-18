fe8b2ff6e6791dbbf195dc374a84ad7a272f6445
Sebastian <sebastiansierra1311@gmail.com>
2023-11-17 20:35:39 -0500

    Se crear una primera version del formulario

--- scripts/main.js
+++ scripts/main.js
@@ -23,12 +23,19 @@ let nets = [];
 let hosts = [];
 let info = {};
 let subnetGenerated = false;
+let departmentCount = 1; // Valor predeterminado
 
 // Eventos que se ejecutan al cargar la pagina
 document.addEventListener("DOMContentLoaded", function() {
     subnetGenerated = false;
     fillSubNetSelect();
     fillNetTypeSelect();
+});
+
+// Nuevo evento al cambiar la cantidad de departamentos
+document.getElementById('departmentCount').addEventListener('change', function () {
+    departmentCount = parseInt(this.value, 10);
+    showDepartmentQuestions(departmentCount);
 });
 
 //llenar la seleccion de sub redes
@@ -81,15 +88,21 @@ subnetBtn.addEventListener('click', () => {
     clearSubNetBtn.classList.remove('hidden');
 });
 
-//evento generar
+
+// Modifica el evento del botón "Generar" para recopilar la información de los departamentos
 generateBtn.addEventListener('click', () => {
-    if(subnetGenerated) {
+    if (subnetGenerated) {
         netType = netTypeSelect.value;
         subnet = subNtwkSelect.value;
         host = hostInput.value;
         showNets();
         showHostRange();
-    }else {
+        // Recopilar información sobre departamentos
+        const departmentInfo = collectDepartmentInfo();
+        console.log(departmentInfo);
+
+        // ... (resto del código)
+    } else {
         errorSubnet.classList.remove('hidden');
     }
 });
@@ -148,4 +161,80 @@ function showHostRange() {
         hostrangeList.appendChild(li);
     });
 }
+
+// Función para mostrar las preguntas adicionales sobre cada departamento
+function showDepartmentQuestions(count) {
+    const departmentQuestionsContainer = document.getElementById('departmentQuestions');
+    departmentQuestionsContainer.innerHTML = ''; // Limpiar el contenedor
+
+    for (let i = 0; i < count; i++) {
+        const departmentDiv = document.createElement('div');
+        departmentDiv.innerHTML = `
+            <h3>Departamento ${i + 1}</h3>
+            <label for="departmentName${i}">Nombre del departamento:</label>
+            <input type="text" id="departmentName${i}">
+
+            <label for="employeeCount${i}">Cantidad de empleados:</label>
+            <input type="number" id="employeeCount${i}" value="1">
+
+            <label for="wifi${i}">¿Crear subred de Wi-Fi en este departamento?</label>
+            <input type="checkbox" id="wifi${i}">
+
+            <label for="networkDevices${i}">Cantidad de dispositivos de red:</label>
+            <select id="networkDevices${i}">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+        `;
+
+        departmentQuestionsContainer.appendChild(departmentDiv);
+    }
+
+    // Mostrar el contenedor de preguntas adicionales
+    departmentQuestionsContainer.classList.remove('hidden');
+}
+
+// Nueva función para recopilar las respuestas sobre los departamentos
+function collectDepartmentInfo() {
+    const departmentInfo = [];
+
+    for (let i = 0; i < departmentCount; i++) {
+        const department = {
+            name: document.getElementById(`departmentName${i}`).value,
+            employeeCount: parseInt(document.getElementById(`employeeCount${i}`).value, 10),
+            wifi: document.getElementById(`wifi${i}`).checked,
+            networkDevices: parseInt(document.getElementById(`networkDevices${i}`).value, 10),
+        };
+
+        departmentInfo.push(department);
+    }
+
+    return departmentInfo;
+}
+
+// Nueva función para guardar la información en un array
+function saveDepartmentInfo() {
+    const departmentInfo = collectDepartmentInfo();
+    // Almacena la información en una constante o variable
+    // En este ejemplo, uso una variable global llamada 'savedDepartmentInfo'
+    savedDepartmentInfo = departmentInfo;
+    console.log("Información de departamentos guardada:", savedDepartmentInfo);
+    alert("Información de departamentos guardada correctamente.");
+}
+// Declara una variable global para almacenar la información de los departamentos
+let savedDepartmentInfo = [];
+
+// Agrega el botón de "Guardar información"
+const saveInfoBtn = document.createElement('button');
+saveInfoBtn.textContent = 'Guardar información';
+saveInfoBtn.addEventListener('click', saveDepartmentInfo);
+document.body.appendChild(saveInfoBtn);
 
