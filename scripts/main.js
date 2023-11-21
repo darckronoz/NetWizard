7ca01073285a73c2a4fd00e8e85241434be53758
darckronoz <crispipe14@gmail.com>
2023-11-20 19:45:22 -0500

    mas limpieza realizada

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,8 @@
 const body = document.body;
 // Obtener elementos del DOM
-const subNtwkSelect = document.getElementById('subNtwk');
-const netTypeSelect = document.getElementById('netType');
-const hostInput = document.getElementById('host');
 const generateBtn = document.getElementById('generate');
-const subnetBtn = document.getElementById('subnetButton');
-const hostResult = document.getElementById('hostrange');
-const subnetsResult = document.getElementById('subnets');
-const subnetsList = document.getElementById('subNetList');
-const hostrangeList = document.getElementById('hostrangeList');
-const errorSubnet = document.getElementById('error_nosubnets');
-const clearSubNetBtn = document.getElementById('clearSubNetList');
 
 //To Do: 
-const initialIP = '192.168.1.1';
-const net_type_select = [10, 66, 128, 192];
 const subnets_select = [1, 2, 4, 8, 16, 32, 64, 128, 256];
 let subnet = 1;
 let netType = 10;
@@ -29,8 +17,6 @@ let subnetClass = 'A'; // Valor predeterminado
 // Eventos que se ejecutan al cargar la pagina
 document.addEventListener("DOMContentLoaded", function() {
     subnetGenerated = false;
-    fillSubNetSelect();
-    fillNetTypeSelect();
 });
 
 // Nuevo evento al cambiar la cantidad de departamentos
@@ -39,56 +25,33 @@ document.getElementById('departmentCount').addEventListener('change', function (
     showDepartmentQuestions(departmentCount);
 });
 
-//llenar la seleccion de sub redes
-function fillSubNetSelect() {
-    subnets_select.forEach(e => {
-        let option = document.createElement('option');
-        option.value = e;
-        option.text = e;
-        subNtwkSelect.appendChild(option);
-    });
-}
-
-//llenar la seleccion de tipos de redes
-function fillNetTypeSelect() {
-    net_type_select.forEach(e => {
-        let option = document.createElement('option');
-        option.value = e;
-        option.text = e;
-        netTypeSelect.appendChild(option);
-    });
-}
-
 //evento generar subredes, esto llena el 
 //diccionario info con: 
 // llave = subnetid
 // valor = cantidad de hosts (queda en 1 hasta que el usuario cambie algun valor)
-subnetBtn.addEventListener('click', () => {
-    subnet = subNtwkSelect.value;
-    subnetsList.innerHTML = ' ';
-    for(let i = 0; i < subnet; i++) {
-        let p = document.createElement('p');
-        let li = document.createElement('li');
-        let input = document.createElement('input');
-        p.textContent = String(i+1) + ': ';
-        input.type = 'number';
-        input.value = '1';
-        input.id = 'subnet'+String(i+1);
-        li.appendChild(p);
-        li.appendChild(input);
-        subnetsList.append(li);
-        info[i+1] = 1;
-        input.addEventListener('change', () => {
-            const inputId = event.target.id;
-            const index = parseInt(inputId.replace('subnet', ''), 10);
-            info[index] = parseInt(event.target.value, 10);
-        });
-    }
-    subnetGenerated = true;
-    errorSubnet.classList.add('hidden');
-    clearSubNetBtn.classList.remove('hidden');
-});
-
+// subnetBtn.addEventListener('click', () => {
+//     subnet = subNtwkSelect.value;
+//     subnetsList.innerHTML = ' ';
+//     for(let i = 0; i < subnet; i++) {
+//         let p = document.createElement('p');
+//         let li = document.createElement('li');
+//         let input = document.createElement('input');
+//         p.textContent = String(i+1) + ': ';
+//         input.type = 'number';
+//         input.value = '1';
+//         input.id = 'subnet'+String(i+1);
+//         li.appendChild(p);
+//         li.appendChild(input);
+//         subnetsList.append(li);
+//         info[i+1] = 1;
+//         input.addEventListener('change', () => {
+//             const inputId = event.target.id;
+//             const index = parseInt(inputId.replace('subnet', ''), 10);
+//             info[index] = parseInt(event.target.value, 10);
+//         });
+//     }
+//     subnetGenerated = true;
+// });
 
 // Modificar el evento del botón "Generar" para recopilar la información de los departamentos
 generateBtn.addEventListener('click', () => {
@@ -113,33 +76,12 @@ generateBtn.addEventListener('click', () => {
 
         // Resto del código después de mostrar las cartas
         // Puedes agregar aquí cualquier lógica adicional que desees ejecutar después de mostrar las cartas.
-
-    } else {
-        errorSubnet.classList.remove('hidden');
-        clearSubNetBtn.classList.remove('hidden'); // Agrega esto si lo necesitas
     }
 });
 
 //ordenar descendentemente por cantidad de hosts.
 function sortSubnetsByHosts() {
     
-}
-
-
-clearSubNetBtn.addEventListener('click', () => {
-    subnetsList.innerHTML = ' ';
-    subnetGenerated = false;
-    clearSubNetBtn.classList.add('hidden');
-});
-
-//función para generar las subredes
-function generateSubNetAddress() {
-    nets = [];
-    const number = 256/subnet;
-    for (let i = 0; i < subnet; i++) {
-        nets.push(number*i);
-    }
-    return nets;
 }
 
 //función para mostrar las sub redes
@@ -278,5 +220,5 @@ let savedDepartmentInfo = [];
 const saveInfoBtn = document.createElement('button');
 saveInfoBtn.textContent = 'Guardar información';
 saveInfoBtn.addEventListener('click', saveDepartmentInfo);
-document.body.appendChild(saveInfoBtn);
+document.getElementById('principal').appendChild(saveInfoBtn);
 
