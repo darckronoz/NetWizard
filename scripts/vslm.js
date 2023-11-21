96f56a5b5c3f750295274da3c05a43d1bf0a092f
Sebastian <sebastiansierra1311@gmail.com>
2023-11-21 09:33:01 -0500

    Correcion de error, direccion de red y broadcasts correctas

--- scripts/vslm.js
+++ scripts/vslm.js
@@ -33,25 +33,26 @@ function vslm(ip, nets) {
     let inicioHost = 1;
 
     for (const [nombre, hosts] of subredesOrdenadas) {
-        const bitsParaHosts = Math.ceil(Math.log2(hosts + 2));  // Se agrega 2 para la dirección de red y de broadcast
+        const bitsParaHosts = Math.ceil(Math.log2(hosts + 2));
         const bitsParaSubred = 32 - bitsParaHosts;
 
         const mascaraSubred = (2 ** bitsParaSubred) - 1;
         const incremento = 2 ** bitsParaHosts;
 
-        const rangoInicio = inicioHost;
-        const rangoFin = inicioHost + incremento - 3;  // Restamos 3 para excluir la dirección de broadcast
+        const rangoInicio = inicioHost + 1;
+        const rangoFin = inicioHost + incremento - 2;
 
         const direccionRed = `${redBase}.${inicioHost}`;
-        const direccionBroadcast = `${redBase}.${rangoFin + 1}`;  // Sumamos 1 para la dirección de broadcast
+        const direccionBroadcast = `${redBase}.${rangoFin + 1}`;
 
-        console.log(`${nombre}: Rango: ${redBase}.${rangoInicio}-${redBase}.${rangoFin}, Dirección de Red: ${direccionRed}, Dirección de Broadcast: ${direccionBroadcast}, Máscara de Subred: /${bitsParaSubred}`);
+        console.log(`${nombre}: Rango: ${redBase}.${rangoInicio}-${redBase}.${rangoFin}, Dirección de Red: ${direccionRed}, Dirección de Broadcast: ${direccionBroadcast}`);
 
         inicioHost += incremento;
     }
 }
 
+
 // Ejemplo de uso
 const ip = '192.168.0.0';
-const subredes = { 'red1': 84, 'red2': 32, 'red3': 40 };
+const subredes = { 'red1': 56, 'red2': 32, 'red3': 40 };
 vslm(ip, subredes);
