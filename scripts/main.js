7eeb6c2fe33e2a125f5e92e67bb26cd52462f21f
darckronoz <crispipe14@gmail.com>
2023-11-22 00:46:34 -0500

    direccionamiento 1 working

--- scripts/main.js
+++ scripts/main.js
@@ -15,13 +15,23 @@ document.addEventListener('DOMContentLoaded', () => {
     if(parseInt(localStorage.getItem('departments')) > 0) {
         departments = parseInt(localStorage.getItem('departments'));
         growthrate = parseInt(localStorage.getItem('growth'))/100;
-        subnetClass = localStorage.getItem('class');
+        subnetClass = setclass(localStorage.getItem('class'));
         createDepartmentsQuestions();
     }else {
         alert('no deberías estar aqui :0');
         window.location.href = "questions.html";
     }
 });
+
+function setclass(letter) {
+    if(letter == 'A') {
+        return '10.0.0.0';
+    }else if(letter == 'B') {
+        return '172.16.0.0';
+    }else {
+        return '192.168.0.0';
+    }
+}
 
 //create departments
 function createDepartmentsQuestions() {
@@ -105,6 +115,8 @@ function lastQuestion() {
     let subnetsdict = convertDepToDictionary();
     console.log('aqui iria el metodo que calcula con este info:');
     console.log(subnetsdict);
+    //vslm(subnetClass, subnetsdict);
+    vslmDOS(subnetClass, subnetsdict);
 }
 
 //pass the net objects to a dictionary
@@ -118,9 +130,150 @@ function convertDepToDictionary() {
     return dict;
 }
 
-//ip = '192.168.0.0'
-//nets = {'red1':24, 'red2':32, 'red3':40}
-function vslm(ip, nets) {
-
+function getFisrt(ipdered) {
+    var first = ipdered.split('.');
+    console.log(first);
+    if(ipdered[3] == 255) {
+        first[3]=0;
+        if(ipdered[2] == 255) {
+            first[2]=0;
+            if(ipdered[1] == 255) {
+                first[1]=0;
+                first[0]++;
+            }
+            first[1]++;
+        }
+        first[2]++;
+    }else{
+        first[3]++;
+    }
+    return first.join('.');
 }
+
+function getLast(broadcast) {
+    var last = broadcast;
+    if(broadcast[3]===0) {
+        if(broadcast[2]===0) {
+            if(broadcast[1]===0) {
+                last[0]--;
+            }
+            last[1]--;
+        }
+        last[2]--;
+    }else {
+        last[3]--;
+    }
+    return last;
+}
+
+//obtiene a partir de un rango el direccionamiento
+function setRange(begin, end) {
+    var a = begin;
+    var b = end;
+
+    return red = {
+        id: a.join('.'),
+        broadcast: b.join('.'),
+    };
+}
+
+//obtener la ultima dirección de ip a partir de una ip y un numero de hosts
+function setLastIp(host, ip) {
+    var ipsplited = ip.split('.');
+    var aux = ip.split('.');
+    for(let i = 0; i < host+2; i++) {
+        if(ipsplited[3] == 255) {
+            ipsplited[2]++;
+            ipsplited[3] = 0;
+        }
+        if(ipsplited[2] == 255) {
+            ipsplited[1]++;
+          ipsplited[2] = 0;
+          ipsplited[3] = 0;
+        }
+        ipsplited[3]++;
+      }
+    
+    return setRange(aux, ipsplited);
+}
+
+//ordenar subredes desc por numero de hosts
+function sortSubnets(subnets) {
+    return Object.entries(subnets)
+    .filter(([_, hosts]) => hosts > 1)
+    .sort((a, b) => b[1] - a[1]);
+}
+
+function vslmDOS(ip, nets) {
+    const subredesOrdenadas = sortSubnets(nets);
+    var newip = ip;
+    var ranges = []
+
+    for(const [nombre, hosts] of subredesOrdenadas) {
+        var aux = newip;
+        var result = setLastIp(hosts, aux);
+        newip = getFisrt(result.broadcast);
+        ranges.push(result);
+    }
+    console.log(ranges);
+}
+
+
+function vslm(ip, nets) {
+    const claseDeRed = localStorage.getItem('class');
+
+    //ordenar redes
+    const subredesOrdenadas = sortSubnets(nets);
+
+    let redBase;
+    if (claseDeRed === 'A') {
+        redBase = ip.split('.').slice(0, 3).join('.');
+    } else if (claseDeRed === 'B') {
+        redBase = ip.split('.').slice(0, 2).join('.');
+    } else if (claseDeRed === 'C') {
+        redBase = ip.split('.').slice(0, 3).join('.');
+    
+        // Validar número máximo de hosts para la clase C
+        const maxHosts = 253;
+        const totalHosts = subredesOrdenadas.reduce((total, [, hosts]) => total + hosts, 0);
+        if (totalHosts > maxHosts) {
+            throw new Error(`El número total de hosts (${totalHosts}) excede el máximo permitido para la clase C (${maxHosts}).`);
+        }
+    }
+    console.log(redBase);
+    let inicioHost = 1;
+
+    for (const [nombre, hosts] of subredesOrdenadas) {
+        const bitsParaHosts = Math.ceil(Math.log2(hosts + 2));
+        const bitsParaSubred = 32 - bitsParaHosts;
+    
+        // Validar desbordamiento de la red para clase A
+        if (claseDeRed === 'A' && inicioHost + 2 ** bitsParaHosts - 2 > 2 ** 24 - 2) {
+            throw new Error(`Desbordamiento de red en subred ${nombre}. El número total de hosts excede el máximo permitido para la clase A.`);
+        }
+    
+        const mascaraSubred = (2 ** bitsParaSubred) - 1;
+        const incremento = 2 ** bitsParaHosts;
+    
+        const rangoInicio = inicioHost ;
+        const rangoFin = inicioHost + incremento - 3;
+    
+        // Validar desbordamiento de la dirección de broadcast para clase A
+        if (claseDeRed === 'A' && inicioHost + incremento - 2 > 2 ** 24 - 2) {
+            throw new Error(`Desbordamiento de dirección de broadcast en subred ${nombre}. El número total de hosts excede el máximo permitido para la clase A.`);
+        }
+    
+        // Validar desbordamiento de la red para clase B
+        if (claseDeRed === 'B' && inicioHost + 2 ** bitsParaHosts - 2 > 2 ** 16 - 2) {
+            throw new Error(`Desbordamiento de red en subred ${nombre}. El número total de hosts excede el máximo permitido para la clase B.`);
+        }
+        const direccionRed = `${redBase}.${inicioHost- 1} `;
+        const direccionBroadcast = `${redBase}.${inicioHost + incremento - 2}`;
+    
+        console.log(`${nombre}: Rango: ${redBase}.${rangoInicio}-${redBase}.${rangoFin}, Dirección de Red: ${direccionRed}, Dirección de Broadcast: ${direccionBroadcast}, mascara: ${bitsParaSubred}`);
+    
+        inicioHost += incremento;
+    }
+}
+
 
