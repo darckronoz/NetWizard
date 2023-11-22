68e431e0a8de9a56d4838a7b61281709d8ad7d02
darckronoz <crispipe14@gmail.com>
2023-11-21 19:55:55 -0500

    cuestionario completado

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,10 @@ const body = document.body;
 // Obtener elementos del DOM
 const generateBtn = document.getElementById('generate');
 const saveInfoBtn = document.getElementById('save-info');
+const divform = document.getElementById('principal');
 
 //To Do: 
-let savedDepartmentInfo = [];
-let ip = '192.168.0.0'; // Valor predeterminado
-let subnets = {};
+let depInfo = [];
 let growthrate = 0;
 let subnetClass = '';
 let departments = 0;
@@ -17,75 +16,107 @@ document.addEventListener('DOMContentLoaded', () => {
         departments = parseInt(localStorage.getItem('departments'));
         growthrate = parseInt(localStorage.getItem('growth'));
         subnetClass = localStorage.getItem('class');
+        createDepartmentsQuestions();
     }else {
         alert('no deberías estar aqui :0');
         window.location.href = "questions.html";
     }
 });
 
-//boton siguiente pregunta.
-saveInfoBtn.addEventListener('click', () => {
-    for (let i = 0; i < departmentCount; i++) {
-        const department = {
-            name: '',
-            employeeCount: 0,
-            wifi: false,
-            networkDevices: 0
+//create departments
+function createDepartmentsQuestions() {
+    for(let i = 0; i < departments; i++) {
+        createQuestion(i);
+    }
+    showQuestion(0);
+}
+
+//create question
+function createQuestion(index) {
+    let questdiv = document.createElement('div');
+    questdiv.innerHTML=`<div class="form hidden" id="quest${index}">
+    <h3>departamento ${index+1}</h3>
+    <br>
+
+    <label for="depname${index}">Nombre del departamento:</label>
+    <input placeholder="name" type="text" id="depname${index}">
+
+    <label for="dephost${index}">Cantidad de empleados:</label>
+    <input type="number" id="dephost${index}" min="1">
+
+    <label for="netdevices${index}">Cantidad de dispositivos de red:</label>
+    <input type="number" id="netdevices${index}" min="1">
+
+    <label for="subnetClass">Red de wifi?:</label>
+    <select id="wifi${index}">
+    <option value="no">no</option>
+    <option value="si">si</option>
+    </select>
+    
+    <button id="next${index}">Siguiente departamento</button>
+    </div>`;
+    divform.appendChild(questdiv);
+}
+
+//save department info
+function saveDepartmentInfo(name, host, wifi, netdevices) {
+    console.log(wifi);
+    const dep1 = {
+        name: name+'-eth',
+        host: host,
+        wifi: wifi,
+        netdevices: netdevices
+    };
+    if(wifi) {
+        const dep2 = {
+            name: name+'-wifi',
+            host: host,
+            wifi: wifi,
+            netdevices: netdevices
         };
-        savedDepartmentInfo.push(department);
+        depInfo.push(dep2);
     }
-    saveDepartmentInfo();
-    window.location.href = "question_card.html";
-});
-
-// Función para mostrar las preguntas adicionales sobre cada departamento
-function showDepartmentQuestions(count) {
-    const departmentQuestionsContainer = document.getElementById('departmentQuestions');
-    departmentQuestionsContainer.innerHTML = ''; // Limpiar el contenedor
-
-    for (let i = 0; i < count; i++) {
-        const departmentDiv = document.createElement('div');
-        departmentDiv.innerHTML = `
-            <h3>Departamento ${i + 1}</h3>
-            <label for="departmentName${i}">Nombre del departamento:</label>
-            <input type="text" id="departmentName${i}">
-
-            <label for="employeeCount${i}">Cantidad de empleados:</label>
-            <input type="number" id="employeeCount${i}" value="1">
-
-            <label for="wifi${i}">¿Crear subred de Wi-Fi en este departamento?</label>
-            <input type="checkbox" id="wifi${i}">
-
-            <label for="networkDevices${i}">Cantidad de dispositivos de red:</label>
-            <select id="networkDevices${i}">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-            </select>
-        `;
-
-        departmentQuestionsContainer.appendChild(departmentDiv);
-    }
-
-    // Mostrar el contenedor de preguntas adicionales
-    departmentQuestionsContainer.classList.remove('hidden');
+    depInfo.push(dep1);
 }
 
-// Nueva función para guardar la información en un array
-function saveDepartmentInfo() {
-
-    console.log("Información de departamentos guardada:", savedDepartmentInfo);
-    alert("Información de departamentos guardada correctamente.");
+//show question with its index
+function showQuestion(index) {
+    let quest = document.getElementById(`quest${index}`);
+    quest.classList.remove('hidden');
+    document.getElementById(`next${index}`).addEventListener('click', () => {
+        let name = document.getElementById(`depname${index}`).value;
+        let hosts = parseInt(document.getElementById(`dephost${index}`).value);
+        hosts = hosts + hosts*growthrate;
+        let netdevices = parseInt(document.getElementById(`netdevices${index}`).value)
+        hosts = hosts + netdevices;
+        let wifi = document.getElementById(`wifi${index}`).value=='si';
+        saveDepartmentInfo(name, hosts, wifi, netdevices);
+        if(index == departments-1) {
+            lastQuestion();
+        }else {
+            quest.classList.add('hidden');
+            showQuestion(index+1);
+        }
+    });
 }
 
+//function for the last question
+function lastQuestion() {
+    let subnetsdict = convertDepToDictionary();
+    console.log('aqui iria el metodo que calcula con este info:');
+    console.log(subnetsdict);
+}
 
+//pass the net objects to a dictionary
+//key: subnetname
+//value: hosts
+function convertDepToDictionary() {
+    let dict = {};
+    depInfo.forEach(dep => {
+        dict[dep.name] = dep.host;
+    });
+    return dict;
+}
 
 //ip = '192.168.0.0'
 //nets = {'red1':24, 'red2':32, 'red3':40}
--- scripts/questions.js
+++ scripts/questions.js
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('class', '');
 });
 
-
 //boton siguiente pregunta.
 saveInfoBtn.addEventListener('click', () => {
     if(parseInt(departmentCount.value) > 0) {
